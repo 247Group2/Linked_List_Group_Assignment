9a20decf74b0802b378a385c292b0f3a4f2957cf
kevinshields <isgroupfa22@gmail.com>
2022-11-19 22:51:53 -0500

    Added class KS Updated #4 requirement Made a separate vehicle data to hold car data cleaned up the main Made more updates Got requirement 2 working

--- src/Main.java
+++ src/Main.java
@@ -37,21 +37,21 @@ public class Main {
 
 
 //      STEP 0
-        System.out.println("\nORIGINAL LINKED LIST:");
+        System.out.println("\nSTEP 0: ORIGINAL LINKED LIST:");
         carList.print(); //prints list
 
 
 //      STEP 1
         System.out.println("\n----------------------------------------------------------");
         carList.insertAtFront(vehicle10); //inserts item at front of list  Req 1 A.S
-        System.out.println("VEHICLE ADDED TO THE FRONT:");
+        System.out.println("STEP 1: VEHICLE ADDED TO THE FRONT:");
         carList.print();
 
 
 //      STEP 2
         System.out.println("\n----------------------------------------------------------");
         carList.removeFromLast();
-        System.out.println("VEHICLE REMOVED FROM BACK:");
+        System.out.println("STEP 2: VEHICLE REMOVED FROM BACK:");
         carList.print();
 
 
@@ -60,8 +60,16 @@ public class Main {
 //      STEP 4
         System.out.println("\n--------------------------------------");
         carList.insertIndex(3, vehicle8) ;
-        System.out.println("VEHICLE ADDED BY INDEXING:");
+        System.out.println("STEP 4: VEHICLE ADDED BY INDEXING:");
         carList.print();
+
+
+//      STEP 5 NEEDS WORK
+        System.out.println("\n----------------------------------------------------------");
+        carList.removeItem(2); //Removes item at specific index, if index is out of bounds prints "Index out of bounds" A.S.
+        System.out.println("STEP 5: Linked List After removing item at index ");
+        carList.print();
+
 
 
 
@@ -75,21 +83,10 @@ public class Main {
         carList.print(); //prints list
 
 
-        System.out.println("----------------------------------------------------------");
-        System.out.println("The linked list after removing item from the end: ");
-//        carList.removeFromLast();
-//        carList.print();
-        System.out.println("Linked list after: Ford2021 Honda2016 ");
 
 
 
 
-
-        System.out.println("----------------------------------------------------------");
-        carList.removeItem(2); //Removes item at specific index, if index is out of bounds prints "Index out of bounds" A.S.
-        System.out.println("Linked List After removing item at index ");
-        carList.print();
-        System.out.println("----------------------------------------------------------");
 
 
 
